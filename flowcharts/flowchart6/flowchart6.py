f67721dd05f792f56bfd492f328651ff6bfbef0d
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-07-12 13:21:32 +0100

    Create flowchart6.py

---
+++ flowcharts/flowchart6/flowchart6.py
@@ -0,0 +1,12 @@
+#Python Flowchart depicting a flow control(conditional) statement
+#This material was used for educational purposes only
+#Many thanks to Al Swegart
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+else:
+    print("You are neither Alice nor a little kid")
