9920214b142e07e80294831c5e03d1a5ae7e4545
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-07-12 13:53:03 +0100

    Create flowchart11.py

---
+++ flowcharts/flowchart11/flowchart11.py
@@ -0,0 +1,7 @@
+#Python Flowchart depicting a flow control(conditional) statement
+#This material was used for educational purposes only
+#Many thanks to Al Swegart
+
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
