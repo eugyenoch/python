b06ddf1161f938874d18f46cc9f8865a4a785dbe
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-07-12 13:38:21 +0100

    Create flowchart8.py

---
+++ flowcharts/flowchart8/flowchart8.py
@@ -0,0 +1,8 @@
+#Python Flowchart depicting a flow control(conditional) statement
+#This material was used for educational purposes only
+#Many thanks to Al Swegart
+
+spam = 0
+while spam < 5:
+    print('Hello, world.')
+    spam = spam + 1
