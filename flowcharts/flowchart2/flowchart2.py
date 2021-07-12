12a57c6b597532c109c4e8736f0c8bf091cfb640
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-07-12 12:17:20 +0100

    Create flowchart2.py

---
+++ flowcharts/flowchart2/flowchart2.py
@@ -0,0 +1,9 @@
+#Python Flowchart depicting a flow control(conditional) statement
+#This material was used for educational purposes only
+#Many thanks to Al Swegart
+
+name = input("What is your name? ")
+if name == 'Alice':
+    print('Hi, Alice.')
+else:
+    print('Hello, stranger.')
