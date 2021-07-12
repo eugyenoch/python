3980d490d618e1daa495ad10eae8f3458d31be96
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-07-12 13:12:03 +0100

    Create flowchart4.py

---
+++ flowcharts/flowchart4/flowchart4.py
@@ -0,0 +1,14 @@
+#Python Flowchart depicting a flow control(conditional) statement
+#This material was used for educational purposes only
+#Many thanks to Al Swegart
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
